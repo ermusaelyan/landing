bad17d1b67e029b9d43d0b79cafe30144a7ce262
ermusaelyan <e.musaelyan98@gmail.com>
2024-08-17 01:25:57 +0400

    styles upd

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,37 +1,70 @@
-import React from 'react';
+'use client'
+
+import React, {useState} from 'react';
 import s from './Menu.module.scss';
 import MenuItem from "@/components/Menu/components/MenuItem/MenuItem.jsx";
 import ArrowIcon from "@/icons/ArrowIcon.jsx";
 import EnglishIcon from "@/icons/EnglishIcon.jsx";
+import classNames from "classnames";
 
-const titles = ['videos', 'websites', 'logos', 'mockups', 'graphics', 'Explore AI'];
+const titles = ['videos', 'websites', 'logos', 'mockups', 'graphics'];
 
 const Menu = () => {
+    const [isNavOpen, setIsNavOpen] = useState(false);
     return (
         <div className={s.wrapper}>
-            <nav className={s.wrapper}>
+            <div className={classNames(s.point, s.create)} onClick={() => setIsNavOpen(prevState => !prevState)}>
+                <div className={s.trigger}>
+                    <div className={s.name}>Create</div>
+                    <div className={classNames(s.caret, {[s.open] : isNavOpen})}><ArrowIcon /></div>
+                </div>
+            </div>
+            <nav className={classNames(s.nav, {[s.open] : isNavOpen})}>
                 <ul className={s.list}>
                     {titles.map(title => (
                         <MenuItem key={title} title={title} />
                     ))}
                 </ul>
             </nav>
-            <div className={s.point}>
-                <a href="#" className={s.point__link}>Pricing</a>
+            <div className={s.ai}>
+                <div className={s.trigger}>
+                    <div className={s.name}>Explore AI</div>
+                    <div className={s.caret}><ArrowIcon /></div>
+                </div>
+                <div className={s.dropdown}>
+                    <ul className={s.ai__list}>
+                        <li className={s.dropdown__item}>
+                            <a href="#" className={s.dropdown__navLink}>Business Name Generator</a>
+                        </li>
+                        <li className={s.dropdown__item}>
+                            <a href="#" className={s.dropdown__navLink}>AI Logo Maker</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
             <div className={s.point}>
-                <span className={s.point__title}>Learn</span>
-                <span className={s.point__icon}>
-                    <ArrowIcon />
-                </span>
+                <div className={s.trigger}>
+                    <a>pricing</a>
+                </div>
             </div>
             <div className={s.point}>
-                <span className={s.point__logo}>
+                <div className={s.trigger}>
+                    <div className={s.name}>Learn</div>
+                    <div className={s.caret}><ArrowIcon /></div>
+                </div>
+            </div>
+            <div className={s.point}>
+                <div className={s.trigger}>
+                <div className={s.language__icon}>
                     <EnglishIcon />
-                </span>
-                <span className={s.point__icon}>
-                    <ArrowIcon />
-                </span>
+                </div>
+                <div className={s.language__name}>English</div>
+                <div className={s.caret}><ArrowIcon /></div>
+            </div>
+            </div>
+            <div className={s.sign}>
+                <button className={s.sign__button}>Sign In</button>
+                <button className={classNames(s.sign__button, s.up)}>Try for Free</button>
             </div>
         </div>
     );
--- src/components/Menu/components/MenuItem/MenuItem.jsx
+++ src/components/Menu/components/MenuItem/MenuItem.jsx
@@ -1,56 +1,20 @@
-import React from 'react';
+'use client'
+
+import React, {useState} from 'react';
 import s from './MenuItem.module.scss';
 import ArrowIcon from "@/icons/ArrowIcon.jsx";
 import VideoIcon from "@/icons/VideoIcon.jsx";
 import BrowseIcon from "@/icons/BrowseIcon.jsx";
 import classNames from "classnames";
 
+const sections = ['Categories', 'Featured'];
+const items = {
+    Categories: 'Music Visualizations',
+    Featured: 'Animated Explainers',
+};
+
 const MenuItem = ({ title }) => {
-    const isAi = title.toLowerCase().includes('ai');
-
-    const renderAiDropdown = () => (
-        <ul className={s.ai__list}>
-            <li className={s.dropdown__item}>
-                <a href="#" className={s.dropdown__navLink}>AI Business Name Generator</a>
-            </li>
-            <li className={s.dropdown__item}>
-                <a href="#" className={s.dropdown__navLink}>AI Logo Maker</a>
-            </li>
-        </ul>
-    );
-
-    const renderDefaultDropdown = () => (
-        <>
-            <div className={classNames(s.dropdown__head, { [s[title]]: title })}>
-                <a className={s.dropdown__link}>
-                    <span className={s.dropdown__icon}><VideoIcon /></span>
-                    <span className={s.dropdown__title}>Create Video</span>
-                </a>
-            </div>
-            <div className={s.dropdown__body}>
-                {['Categories', 'Featured'].map((section) => (
-                    <div className={s.dropdown__wrap} key={section}>
-                        <div className={s.dropdown__subtitle}>{section}</div>
-                        <ul className={s.dropdown__list}>
-                            {Array.from({ length: 10 }, (_, i) => (
-                                <li className={s.dropdown__item} key={i}>
-                                    <a href="#" className={s.dropdown__navLink}>
-                                        {section === 'Categories' ? 'Music Visualizations' : 'Animated Explainers'}
-                                    </a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
-            </div>
-            <div className={s.dropdown__footer}>
-                <a className={s.dropdown__browseLink}>
-                    <span className={s.browse__title}>Browse More</span>
-                    <span className={s.browse__icon}><BrowseIcon /></span>
-                </a>
-            </div>
-        </>
-    );
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <li className={s.wrapper}>
@@ -59,11 +23,48 @@ const MenuItem = ({ title }) => {
                 <div className={s.caret}><ArrowIcon /></div>
             </div>
             <div className={s.dropdown}>
-                {isAi ? renderAiDropdown() : renderDefaultDropdown()}
+                <div className={classNames(s.dropdown__head, { [s[title]]: title })}
+                     onClick={() => setIsOpen(true)}>
+                    <a className={s.dropdown__link}>
+                        <span className={s.dropdown__icon}><VideoIcon /></span>
+                        <span className={s.dropdown__title}>Create Video</span>
+                    </a>
+                </div>
+                <div className={classNames(s.wrap, {[s.open]: isOpen })}>
+                    <div className={s.dropdown__body}>
+                        <div className={s.side}>
+                            <button className={s.side__button} onClick={() => setIsOpen(false)}>
+                            <span className={s.side__icon}>
+                            <ArrowIcon />
+                        </span>
+                            </button>
+                            <a href="#" className={s.side__link}>Create video</a>
+                        </div>
+                        {sections.map(section => (
+                            <div className={s.dropdown__wrap} key={section}>
+                                <div className={s.dropdown__subtitle}>{section}</div>
+                                <ul className={s.dropdown__list}>
+                                    {Array.from({ length: 10 }).map((_, i) => (
+                                        <li className={s.dropdown__item} key={i}>
+                                            <a href="#" className={s.dropdown__navLink}>{items[section]}</a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
+                    </div>
+                    <div className={s.dropdown__footer}>
+                        <a className={s.dropdown__browseLink}>
+                            <span className={s.browse__title}>Browse More</span>
+                            <span className={s.browse__icon}><BrowseIcon /></span>
+                        </a>
+                    </div>
+                </div>
             </div>
         </li>
     );
-};
+}
 
 export default MenuItem;
+
 
